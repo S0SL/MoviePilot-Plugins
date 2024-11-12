e04ef90a17dccabbdddc1379301796b180c0083b
S0SL <16398874+S0SL@users.noreply.github.com>
2024-11-12 14:48:50 +0800

    Update __init__.py

--- plugins/serverchanmsg/__init__.py
+++ plugins/serverchanmsg/__init__.py
@@ -14,7 +14,7 @@ class ServerChanMsg(_PluginBase):
     # 插件图标
     plugin_icon = "ServerChen.png"
     # 插件版本
-    plugin_version = "1.5"
+    plugin_version = "1.56"
     # 插件作者
     plugin_author = "SOSL"
     # 作者主页
