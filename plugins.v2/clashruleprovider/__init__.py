48d501222a224094e814471af2ced7b635f040a0
S0SL <16398874+S0SL@users.noreply.github.com>
2025-06-14 02:05:43 +0800

    更新 __init__.py

--- plugins.v2/clashruleprovider/__init__.py
+++ plugins.v2/clashruleprovider/__init__.py
@@ -29,7 +29,7 @@ class ClashRuleProvider(_PluginBase):
     plugin_icon = ("https://raw.githubusercontent.com/wumode/MoviePilot-Plugins/"
                    "refs/heads/imdbsource_assets/icons/Mihomo_Meta_A.png")
     # 插件版本
-    plugin_version = "0.1.0"
+    plugin_version = "0.1.1"
     # 插件作者
     plugin_author = "wumode"
     # 作者主页
